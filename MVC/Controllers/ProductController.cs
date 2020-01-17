0fd1da05bf8398cb7c5f10a815774d88855223de
Jaka <jax.atlet@hotmail.com>
2020-01-17 12:43:07 +0100

    first commit

---
+++ MVC/Controllers/ProductController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            IEnumerable<mvcProductModel> prodList;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Product").Result;
+            prodList = response.Content.ReadAsAsync<IEnumerable<mvcProductModel>>().Result;
+            return View(prodList);
+        }
+
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+            return View(new mvcProductModel());
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Product/"+id.ToString()).Result;
+                return View(response.Content.ReadAsAsync<mvcProductModel>().Result);
+            }
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(mvcProductModel prod)
+        {
+            if (prod.ProductID == 0) 
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("Product", prod).Result;
+                TempData["SuccessMessage"] = "Saved Successfully";
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PutAsJsonAsync("Product/"+prod.ProductID, prod).Result;
+                TempData["SuccessMessage"] = "Updated Successfully";
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.DeleteAsync("Product/" + id.ToString()).Result;
+            TempData["SuccessMessage"] = "Deleted Successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVC/Models/mvcProductModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Models
+{
+    public class mvcProductModel
+    {
+        public int ProductID { get; set; }
+
+        [Required(ErrorMessage = "This Field is Required")]
+        public string Name { get; set; }
+        public Nullable<int> Price { get; set; }
+        public Nullable<int> Stock { get; set; }
+        public Nullable<int> Code { get; set; }
+
+    }
+}
